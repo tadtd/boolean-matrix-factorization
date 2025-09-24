7458affd1da711948a8fb69c4211333a81099c15
tadtd <tiendat1243oo@gmail.com>
2025-09-24 20:54:06 +0700

    feat: add data processing scripts for mushroom dataset and refactor GreCon and GreConD test cases

--- test/grecon.py
+++ test/grecon.py
@@ -1,15 +1,23 @@
-from BMF import GreCon
+from BMF import GreCon, utils
 import numpy as np
+from ucimlrepo import fetch_ucirepo 
+from sklearn.preprocessing import OneHotEncoder
 
-X = np.array([
-    [0, 0, 1, 0, 0, 0],
-    [0, 0, 1, 0, 0, 0],
-    [0, 1, 0, 1, 0, 0],
-    [0, 0, 1, 0, 0, 1],
-    [0, 1, 1, 1, 0, 1],
-])
+def main():
+  model = GreCon()
+  mushroom = fetch_ucirepo(id=73) 
 
-model = GreCon()
-res = model.solve(X)
-print(res)
-res.show_factors()
+  X = mushroom.data.features 
+
+  encoder = OneHotEncoder(sparse_output=False, dtype=int)
+  X_bin = encoder.fit_transform(X)
+  X_bin = X_bin.astype(int)
+
+  Y = utils.random_boolean_matrix(100, 100, density=0.1, random_state=42)
+
+  res = model.solve(Y)
+  # res.show_factors()
+  print(res)
+
+if __name__ == "__main__":
+  main()
--- test/grecond.py
+++ test/grecond.py
@@ -1,16 +1,24 @@
-from BMF import GreConD
+from BMF import GreConD, utils
 import numpy as np
+from ucimlrepo import fetch_ucirepo 
+from sklearn.preprocessing import OneHotEncoder
 
-model = GreConD()
-X = np.array([
-    [0, 0, 1, 0, 0, 0],
-    [0, 0, 1, 0, 0, 0],
-    [0, 1, 0, 1, 0, 0],
-    [0, 0, 1, 0, 0, 1],
-    [0, 1, 1, 1, 0, 1],
-])
+def main():
+  model = GreConD()
+  mushroom = fetch_ucirepo(id=73) 
 
-res = model.solve(X)
-print(res)
-res.show_factors()
-# print(res.reconstruction)
+    # data (as pandas dataframes) 
+  X = mushroom.data.features 
+  
+  encoder = OneHotEncoder(sparse_output=False, dtype=int)
+  X_bin = encoder.fit_transform(X)
+  X_bin = X_bin.astype(int)
+
+  Y = utils.random_boolean_matrix(80, 50, density=0.1, random_state=42)
+  res = model.solve(Y)
+  print(res)
+  res.show_factors()
+  # print(res.reconstruction)
+
+if __name__ == "__main__":
+  main()
